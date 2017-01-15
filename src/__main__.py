ab162237d009af032bf33663a46db6827865527e
Vasantha Ganesh K <vasanthaganesh.k@tuta.io>
2017-01-16 00:30:03 +0530

    paths can be relative for the bots, the have to be defined in bots_config.json in new lines

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,14 @@
 from engine.gameloop import gameloop
+from json import loads
+import os
 
 if __name__ == "__main__":
     print "="*80
     print "Starting Engine".center(80)
-    print "="*80    
+    print "="*80
 
-    gameloop([['/usr/bin/python', 'python', '/home/homonculus/Code/Projects/phoenix/bots/bot1.py']])
+    bots_config_dir = os.path.dirname(os.path.realpath('__file__'))
+    a = open(os.path.join(bots_config_dir, "bots_config.json"), 'r')
+    bot_start_config = map(loads, a.read().strip().split('\n'))
+
+    gameloop(bot_start_config)
