38aa8000612bb7f20a888f5b3817a92784df014c
Saranya Kolanu <saranya.kolanu@gmail.com>
2017-02-20 14:39:31 +0530

    Update gen_map.py
    Updated the range of the random functions and changed the number of food, viruses and force fields.

--- maps/gen_map.py
+++ maps/gen_map.py
@@ -1,9 +1,9 @@
-import random
+from random import randrange
 from collections import defaultdict
 
 def generate_map(old_map):
   new_map={
-  "virusrad": 15,
+  "virusrad": 35,
   "maxX": 14142,
   "maxY": 14142,
   "ffieldcircle": [],
@@ -11,21 +11,23 @@ def generate_map(old_map):
   "virus": [],
   "ffieldsquare": []
   }
-  #generates a map of 10 food,5 viruses,2 force fields of square and circle each
-  for i in range(10):
-      new_map["food"].append([random.randrange(0,14142),random.randrange(0,14142)])
-  for i in range(5):
-      new_map["virus"].append([random.randrange(0,14142),random.randrange(0,14142)])
-  for i in range(2):
+  no_of_food = randrange(50,80)
+  for i in xrange(no_of_food):
+      new_map["food"].append([randrange(0,14142),randrange(0,14142)])
+  no_of_virus = randrange(50,80)
+  for i in xrange(no_of_virus):
+      new_map["virus"].append([randrange(0,14142),randrange(0,14142)])
+  no_of_ffields = randrange(20,40)
+  for i in xrange(no_of_ffields):
       fsquare=defaultdict()
-      fsquare["origin"]=[random.randrange(0,14142),random.randrange(0,14142)]
-      fsquare["innerrad"]=random.randrange(1,75)
-      fsquare["outerrad"]=random.randrange(1,75)
+      fsquare["origin"]=[randrange(0,14050),randrange(0,14050)]
+      fsquare["innerside"]=randrange(1,50)
+      fsquare["outerside"]=randrange(51,80)
       new_map["ffieldsquare"].append(fsquare)
       fcircle=defaultdict()
-      fcircle["origin"]=[random.randrange(0,14142),random.randrange(0,14142)]
-      fcircle["innerrad"]=random.randrange(0,14142)
-      fcircle["outerrad"]=random.randrange(0,14142)
+      fcircle["origin"]=[randrange(0,14050),randrange(0,14050)]
+      fcircle["innerrad"]=randrange(1,50)
+      fcircle["outerrad"]=randrange(51,75)
       new_map["ffieldsquare"].append(fcircle)
 
   return new_map
