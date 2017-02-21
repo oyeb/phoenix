dd4eca818b79f1262e41382f3a4c7cd634e0f6eb
Gautam Sudesh <gtmsdsh@gmail.com>
2017-02-21 12:28:38 +0000

    added system calls filter

--- src/engine/syscall_filter.py
+++ src/engine/syscall_filter.py
@@ -18,8 +18,123 @@ def syscall_filter():
     fltr.add_rule(seccomp.KILL, 'clone')
     fltr.add_rule(seccomp.KILL, 'fork')
     
-    #gives info about the file
+    #gives status about the file
     fltr.add_rule(seccomp.KILL, 'stat')
+    fltr.add_rule(seccomp.KILL, 'fstat')
+    fltr.add_rule(seccomp.KILL, 'lstat')
+    
+    #repostions offset of the open file
+    fltr.add_rule(seccomp.KILL, 'lseek')
+    
+    #examine and change a signal action 
+    fltr.add_rule(seccomp.KILL, 'rt_sigaction')
+    
+    #examine and change blocked signals 
+    fltr.add_rule(seccomp.KILL, 'rt_sigprocmask')
+    
+    #return from signal handler and cleanup stack frame
+    fltr.add_rule(seccomp.KILL, 'rt_sigreturn')
+    
+    #manipulates device parameters of special files
+    fltr.add_rule(seccomp.KILL, 'ioctl')
+    
+    #create pipe for file descriptors
+    fltr.add_rule(seccomp.KILL, 'pipe')
+    
+    #remap a virtual memory address
+    fltr.add_rule(seccomp.KILL, 'mremap')
+    
+    #synchronize a file with a memory map
+    fltr.add_rule(seccomp.KILL, 'msync')
+     
+    #give advice about use of memory 
+    fltr.add_rule(seccomp.KILL, 'madvise')
+     
+    #shared memory 
+    fltr.add_rule(seccomp.KILL, 'shmget')
+    fltr.add_rule(seccomp.KILL, 'shmat')
+    fltr.add_rule(seccomp.KILL, 'shmctl')
+    fltr.add_rule(seccomp.KILL, 'shmdt')
+    
+    #duplicate file descriptor
+    fltr.add_rule(seccomp.KILL, 'dup')
+    fltr.add_rule(seccomp.KILL, 'dup2')
+    
+    #wait for signal
+    fltr.add_rule(seccomp.KILL, 'pause')
+    
+    # high-resolution sleep 
+    fltr.add_rule(seccomp.KILL, 'nanosleep')
+    
+    #get or set value of interval timer
+    fltr.add_rule(seccomp.KILL, 'getitimer')
+    fltr.add_rule(seccomp.KILL, 'setitimer')
+    
+    # set an alarm for delivery of a signal 
+    fltr.add_rule(seccomp.KILL, 'alarm')
+    
+    #transfer data between file descriptors
+    fltr.add_rule(seccomp.KILL, 'sendfile')
+    
+    #create an endpoint for communication
+    fltr.add_rule(seccomp.KILL, 'socket')
+    
+    #initiate a connection on a socket
+    fltr.add_rule(seccomp.KILL, 'connect')
+    
+    #accept connection or send message or receive on a socket
+    fltr.add_rule(seccomp.KILL, 'accept')
+    fltr.add_rule(seccomp.KILL, 'sendto')
+    fltr.add_rule(seccomp.KILL, 'recvfrom')
+    fltr.add_rule(seccomp.KILL, 'sendmsg')
+    fltr.add_rule(seccomp.KILL, 'recvmsg')
+    
+    #other socket related system calls
+    fltr.add_rule(seccomp.KILL, 'shutdown')
+    fltr.add_rule(seccomp.KILL, 'bind')
+    fltr.add_rule(seccomp.KILL, 'listen')
+    fltr.add_rule(seccomp.KILL, 'getsockname')
+    fltr.add_rule(seccomp.KILL, 'getpeername')
+    fltr.add_rule(seccomp.KILL, 'socketpair')
+    fltr.add_rule(seccomp.KILL, 'setsockopt')
+    fltr.add_rule(seccomp.KILL, 'getsockopt')
+    
+    #system calls for child process
+    fltr.add_rule(seccomp.KILL, 'clone')
+    fltr.add_rule(seccomp.KILL, 'fork')
+    fltr.add_rule(seccomp.KILL, 'vfork')
+    
+    #execute programs
+    fltr.add_rule(seccomp.KILL, 'execve')
+    
+    #gives info about kernel
+    fltr.add_rule(seccomp.KILL, 'uname')
+    
+    #message related sys calls (IPCs)
+    fltr.add_rule(seccomp.KILL, 'msgget')
+    fltr.add_rule(seccomp.KILL, 'msgsnd')
+    fltr.add_rule(seccomp.KILL, 'msgrcv')
+    fltr.add_rule(seccomp.KILL, 'msgctl')
+    
+    #manipulate file descriptor
+    fltr.add_rule(seccomp.KILL, 'fcntl')
+    
+    #apply or remove an advisory lock on an open file
+    fltr.add_rule(seccomp.KILL, 'flock')
+    
+    # truncate a file to a specified length
+    fltr.add_rule(seccomp.KILL, 'truncate')
+    fltr.add_rule(seccomp.KILL, 'ftruncate')
+    
+    #directory sys calls 
+    fltr.add_rule(seccomp.KILL, 'getdents')
+    fltr.add_rule(seccomp.KILL, 'getcwd')
+    fltr.add_rule(seccomp.KILL, 'chdir')
+    fltr.add_rule(seccomp.KILL, 'fchdir')
+    fltr.add_rule(seccomp.KILL, 'mkdir')
+    fltr.add_rule(seccomp.KILL, 'rmdir')
+    
+    
     fltr.load()
 
     
