73532d3898c9244dcfa5d3a27a36c5e64c71623e
Saranya Kolanu <saranya.kolanu@gmail.com>
2017-02-21 21:31:33 +0530

    Update gen_map.py
    Changed the dimensions to 5000*5000

--- maps/gen_map.py
+++ maps/gen_map.py
@@ -4,8 +4,8 @@ from collections import defaultdict
 def generate_map(old_map):
   new_map={
   "virusrad": 35,
-  "maxX": 14142,
-  "maxY": 14142,
+  "maxX": 5000,
+  "maxY": 5000,
   "ffieldcircle": [],
   "food": [],
   "virus": [],
@@ -13,19 +13,19 @@ def generate_map(old_map):
   }
   no_of_food = randrange(50,80)
   for i in xrange(no_of_food):
-      new_map["food"].append([randrange(0,14142),randrange(0,14142)])
+      new_map["food"].append([randrange(0,5000),randrange(0,5000)])
   no_of_virus = randrange(50,80)
   for i in xrange(no_of_virus):
-      new_map["virus"].append([randrange(0,14142),randrange(0,14142)])
+      new_map["virus"].append([randrange(0,5000),randrange(0,5000)])
   no_of_ffields = randrange(20,40)
   for i in xrange(no_of_ffields):
       fsquare=defaultdict()
-      fsquare["origin"]=[randrange(0,14050),randrange(0,14050)]
+      fsquare["origin"]=[randrange(0,4900),randrange(0,4900)]
       fsquare["innerside"]=randrange(1,50)
       fsquare["outerside"]=randrange(51,80)
       new_map["ffieldsquare"].append(fsquare)
       fcircle=defaultdict()
-      fcircle["origin"]=[randrange(0,14050),randrange(0,14050)]
+      fcircle["origin"]=[randrange(0,4900),randrange(0,4900)]
       fcircle["innerrad"]=randrange(1,50)
       fcircle["outerrad"]=randrange(51,75)
       new_map["ffieldsquare"].append(fcircle)
