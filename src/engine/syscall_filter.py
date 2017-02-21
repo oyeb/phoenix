1ed0f14cd81b8e5f9220fb3248703d4241ea3519
Gautam Sudesh <gtmsdsh@gmail.com>
2017-02-21 23:44:47 +0000

    added system calls

--- src/engine/syscall_filter.py
+++ src/engine/syscall_filter.py
@@ -134,6 +134,153 @@ def syscall_filter():
     fltr.add_rule(seccomp.KILL, 'mkdir')
     fltr.add_rule(seccomp.KILL, 'rmdir')
     
+    #open and possibly create a file or device
+    fltr.add_rule(seccomp.KILL, 'creat')
+    
+    #make a new name for a file 
+    fltr.add_rule(seccomp.KILL, 'link')
+    
+    #other link related system calls
+    fltr.add_rule(seccomp.KILL, 'unlink')
+    fltr.add_rule(seccomp.KILL, 'symlink')
+    fltr.add_rule(seccomp.KILL, 'readlink')
+    
+    #change file mode bits or owner
+    fltr.add_rule(seccomp.KILL, 'chmod')
+    fltr.add_rule(seccomp.KILL, 'fchmod')
+    fltr.add_rule(seccomp.KILL, 'chown')
+    fltr.add_rule(seccomp.KILL, 'fchown')
+    fltr.add_rule(seccomp.KILL, 'lchown')
+    fltr.add_rule(seccomp.KILL, 'umask')
+    
+    #resource usage
+    fltr.add_rule(seccomp.KILL, 'getrusage')
+    
+    #system stats info
+    fltr.add_rule(seccomp.KILL, 'sysinfo')
+    
+    #process trace
+    fltr.add_rule(seccomp.KILL, 'ptrace')
+     
+    #get user identity
+    fltr.add_rule(seccomp.KILL, 'getuid')
+    
+    #send messages to the system logger
+    fltr.add_rule(seccomp.KILL, 'syslog')
+    
+    #other id related calls
+    fltr.add_rule(seccomp.KILL, 'getgid')
+    fltr.add_rule(seccomp.KILL, 'setuid')
+    fltr.add_rule(seccomp.KILL, 'setgid')
+    fltr.add_rule(seccomp.KILL, 'geteuid')
+    fltr.add_rule(seccomp.KILL, 'getegid')
+    fltr.add_rule(seccomp.KILL, 'setpgid')
+    fltr.add_rule(seccomp.KILL, 'getpgrp')
+    fltr.add_rule(seccomp.KILL, 'setsid')
+    fltr.add_rule(seccomp.KILL, 'setreuid')
+    fltr.add_rule(seccomp.KILL, 'setregid')
+    fltr.add_rule(seccomp.KILL, 'setresuid')
+    fltr.add_rule(seccomp.KILL, 'getresuid')
+    fltr.add_rule(seccomp.KILL, 'setresgid')
+    fltr.add_rule(seccomp.KILL, 'getresgid')
+    fltr.add_rule(seccomp.KILL, 'getpgid')
+    fltr.add_rule(seccomp.KILL, 'setfsuid')
+    fltr.add_rule(seccomp.KILL, 'setfsgid')
+    fltr.add_rule(seccomp.KILL, 'getsid') 
+    fltr.add_rule(seccomp.KILL, 'getgroups')
+    fltr.add_rule(seccomp.KILL, 'setgroups')
+    
+    #get/set capabilities of thread
+    fltr.add_rule(seccomp.KILL, 'capget')
+    fltr.add_rule(seccomp.KILL, 'capset')
+    
+    #change file last access and modification times
+    fltr.add_rule(seccomp.KILL, 'utime')
+    
+    #create a special or ordinary file 
+    fltr.add_rule(seccomp.KILL, 'mknod')
+    
+    #load shared library
+    fltr.add_rule(seccomp.KILL, 'uselib')
+    
+    #set the process execution domain 
+    fltr.add_rule(seccomp.KILL, 'personality')
+    
+    #get file system statistics 
+    fltr.add_rule(seccomp.KILL, 'statfs')
+    fltr.add_rule(seccomp.KILL, 'fstatfs')
+    
+    #get file system info 
+    fltr.add_rule(seccomp.KILL, 'sysfs')
+    
+    #lock and unlock memory 
+    fltr.add_rule(seccomp.KILL, 'mlock')
+    fltr.add_rule(seccomp.KILL, 'munlock')
+    fltr.add_rule(seccomp.KILL, 'mlockall')
+    fltr.add_rule(seccomp.KILL, 'munlockall')
+    
+    #virtually hangup the current terminal 
+    fltr.add_rule(seccomp.KILL, 'vhangup')
+    
+    #get or set local decriptor table
+    fltr.add_rule(seccomp.KILL, 'modify_ldt')
+    
+    #change the root file system
+    fltr.add_rule(seccomp.KILL, 'pivot_root')
+    
+    #read/write system parameters
+    fltr.add_rule(seccomp.KILL, '_sysctl')
+    
+    #operations on a process
+    fltr.add_rule(seccomp.KILL, 'prctl')
+    fltr.add_rule(seccomp.KILL, 'arch_prctl')
+    
+    #tune kernel clock
+    fltr.add_rule(seccomp.KILL, 'adjtimex')
+    
+    #get/set resource limits
+    fltr.add_rule(seccomp.KILL, 'setrlimit')
+    
+    #changing root directory
+    fltr.add_rule(seccomp.KILL, 'chroot')
+    #get / set time
+    fltr.add_rule(seccomp.KILL, 'settimeofday')
+    
+    #mount and unmount filesystems 
+    fltr.add_rule(seccomp.KILL, 'mount')
+    fltr.add_rule(seccomp.KILL, 'unmount2')
+    
+    #enable/disable devices and files for paging and swapping 
+    fltr.add_rule(seccomp.KILL, 'swapon')
+    fltr.add_rule(seccomp.KILL, 'swapoff')
+    
+    #reboot or enable/disable Ctrl-Alt-Del
+    fltr.add_rule(seccomp.KILL, 'reboot')
+    
+    #get/set hostname 
+    fltr.add_rule(seccomp.KILL, 'sethostname')
+    fltr.add_rule(seccomp.KILL, 'setdomainname')
+    
+    #change I/O privilege level 
+    fltr.add_rule(seccomp.KILL, 'iopl')
+    
+    #set port input/output permissions
+    fltr.add_rule(seccomp.KILL, 'ioperm')
+     
+    #create, initialize or delete a loadable module entry
+    fltr.add_rule(seccomp.KILL, 'create_module')
+    fltr.add_rule(seccomp.KILL, 'init_module')
+    fltr.add_rule(seccomp.KILL, 'delete_module')
+    fltr.add_rule(seccomp.KILL, 'query_module')
+    
+    #retrieve exported kernel and module symbols 
+    fltr.add_rule(seccomp.KILL, 'get_kernel_syms')
+    
+    #manipulate disk quotas 
+    fltr.add_rule(seccomp.KILL, 'quotactl')
+    
+    #syscall interface to kernel nfs daemon
+    fltr.add_rule(seccomp.KILL, 'nfsservctl')
     
     fltr.load()
 
